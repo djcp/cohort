0a659e1e0fb5cdd25fe3ce9e555efc9fbd4dfcbe
djcp <djcp@0f18bc3b-1a62-0410-b1a6-bc94dc643be9>
2009-03-31 14:35:52 +0000

    fix saved searches to properly namespace between global and personal searches.
    git-svn-id: https://adam/svn/cohort/trunk@56 0f18bc3b-1a62-0410-b1a6-bc94dc643be9

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -8,13 +8,15 @@ class SavedSearch < ActiveRecord::Base
     if global_search == false
       category_conditions << 'select category from saved_searches where global_search is false and user_id = ? group by category order by category'
       category_conditions << user_id
+      search_conditions << 'global_search is false and category = ? and user_id = ' + self.connection.quote(user_id)
     else
       category_conditions << 'select category,count(category) from saved_searches where global_search is true group by category order by category'
+      search_conditions << 'category = ? and global_search is true'
     end
     search_cat = self.find_by_sql(category_conditions)
     grouped_options = [['',['-- select one --']]]
     search_cat.each do|cat|
-      searches_in_cat = self.find(:all, :conditions => ['category = ? and global_search = ?', cat.category, global_search ], :order => :name)
+      searches_in_cat = self.find(:all, :conditions => [search_conditions, cat.category].flatten, :order => :name)
       grouped_options << [cat.category,searches_in_cat.collect{|ss| [ss.name, ss.id.to_s]}]
     end
     return grouped_options
