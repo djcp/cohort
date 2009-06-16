9e37ddf80526a441725f806730cb024c2b64fc2d
Ryan Neufeld <neufelry@gmail.com>
2009-06-16 09:42:40 -0500

    updated failing spec with note

--- spec/models/tag_duplication_spec.rb
+++ spec/models/tag_duplication_spec.rb
@@ -4,8 +4,8 @@ describe Tagging do
   it "should not create duplicate taggings" do
     t = Tag.create(:title => 'auto tag')
     c = Contact.new
+    c.save # MUST be here, otherwise rails bug kicks in and this fails
     c.tags << t
-
     lambda { c.save }.should_not raise_error
   end
 end
