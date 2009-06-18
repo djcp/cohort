ad9ca04de00718186aeb1b0ea659d888329d3199
Daniel Collis-Puro <dan@collispuro.net>
2009-06-18 09:45:25 -0400

    Fix tag editing.

--- vendor/plugins/acts_as_freetaggable_cohort_extensions/init.rb
+++ vendor/plugins/acts_as_freetaggable_cohort_extensions/init.rb
@@ -6,6 +6,10 @@ Tag.class_eval do
   has_many :log_items, :as => :item, :dependent => :destroy
 
   before_destroy :allow_delete_of_removable_objects
+
+  def name_for_display
+    "#{self.hierarchical_title}"
+  end
   
   def move_higher
     self.move_up
