58d0d4ebc891e153dbc8fcb47f1e0e481d8d2f39
Daniel Collis-Puro <dan@collispuro.net>
2009-07-01 10:17:59 -0400

    Significant performance improvements to the dashboard listings.

--- lib/cohort_ar_class_mixin.rb
+++ lib/cohort_ar_class_mixin.rb
@@ -1,20 +1,40 @@
 module CohortArClassMixin
 
   def recent_adds(limit = 5)
-    self.find(:all, :order => 'created_at desc', :limit => limit)
+    includes = []
+    includes = self.default_includes_by_class
+    self.find(:all, :order => 'created_at desc', :include => includes, :limit => limit)
   end
 
   def my_recent_adds(user_id, limit = 5)
     return nil if user_id.blank?
-    self.find(:all, :conditions => ['user_id = ?',user_id], :order => 'created_at desc', :limit => limit)
+    includes = self.default_includes_by_class
+    self.find(:all, :conditions => ['user_id = ?',user_id], :include => includes, :order => 'created_at desc', :limit => limit)
   end
 
   def recent_updates(limit = 5)
-    self.find(:all, :order => 'updated_at desc', :limit => limit)
+    includes = self.default_includes_by_class
+    self.find(:all, :order => 'updated_at desc', :include => includes, :limit => limit)
   end
 
   def my_recent_updates(user_id, limit = 5)
     return nil if user_id.blank?
-    self.find(:all, :conditions => ['user_id = ?',user_id], :order => 'updated_at desc', :limit => limit)
+    includes = self.default_includes_by_class
+    self.find(:all, :conditions => ['user_id = ?',user_id], :include => includes, :order => 'updated_at desc', :limit => limit)
   end
+
+  protected
+
+  def default_includes_by_class
+    includes = []
+    if self.class_name == 'Note'
+      includes = [:contact,:user]
+    elsif self.class_name == 'Contact'
+      includes = [:contact_emails, :notes, :tags]
+    elsif self.class_name == 'Tagging'
+      includes = [:freetaggable, :tag]
+    end
+    return includes
+  end
+
 end
