72f46135b5f1380f6caee2007cc361266d8054e9
Daniel Collis-Puro <dan@collispuro.net>
2010-04-28 14:17:19 -0400

    delayed_job updates.

--- Rakefile
+++ Rakefile
@@ -8,3 +8,8 @@ require 'rake/testtask'
 require 'rake/rdoctask'
 
 require 'tasks/rails'
+begin
+  require 'delayed/tasks'
+rescue LoadError
+  STDERR.puts "Run `rake gems:install` to install delayed_job"
+end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,10 +21,10 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
+config.gem "shoulda", :version => '2.10.3'
 
-config.gem "rspec",            :lib => false
-config.gem "rspec-rails",      :lib => false
-config.gem "remarkable_rails", :lib => false
+config.gem "rspec",            :version => '1.3.0'
+config.gem "rspec-rails",      :version => '1.3.2'
+config.gem "remarkable_rails", :version => '3.1.13'
 
-config.gem "thoughtbot-factory_girl", :lib => 'factory_girl', :source => 'http://gems.github.com'
+config.gem "factory_girl", :version => '1.2.4'
